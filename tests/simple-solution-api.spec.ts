416d22093b66110412582cb8e8a12157af50d867
aleksei.golov <lelik.llk@gmail.com>
2024-10-16 13:34:11 +0300

    HW09 - Added tests , prettier hookup and one Axios test for put

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test'
-
 import { StatusCodes } from 'http-status-codes'
+import axios, { AxiosError } from 'axios'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
@@ -56,3 +56,148 @@ test('post order with incorrect data should receive code 400', async ({ request 
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
+
+test('PUT: Correct ID and valid key updates order and returns correct code', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'test',
+    customerPhone: 'test',
+    comment: 'test',
+    id: 0,
+  }
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/1', {
+    headers: requestHeaders,
+    data: requestBody,
+  })
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('PUT : Invalid  api-key is rejected as unauthorised    ', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '123456786',
+  }
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'test',
+    customerPhone: 'test',
+    comment: 'test',
+    id: 0,
+  }
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/1', {
+    headers: requestHeaders,
+    data: requestBody,
+  })
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('PUT : Correct return code is returned in case orderID is out of range (BAD_REQUEST) ', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'test',
+    customerPhone: 'test',
+    comment: 'test',
+    id: 0,
+  }
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/11', {
+    headers: requestHeaders,
+    data: requestBody,
+  })
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+test('DELETE: Correct ID and valid key deletes order and returns success code 204(NO_CONTENT) ', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/9', {
+    headers: requestHeaders
+  })
+  expect(response.status()).toBe(StatusCodes.NO_CONTENT)
+})
+
+test('DELETE: Request without valid api-key is rejected with correct return code ', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '123456890123456',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/9', {
+    headers: requestHeaders
+  })
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('DELETE: Invalid order ID fails as bad request with correct return code ', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/0', {
+    headers: requestHeaders
+  })
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+
+test('GET: Correct error code is returned if username and password are correctly set with api-key', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders?username=username&password=password', {
+    headers: requestHeaders
+  })
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('GET: Correct error code is returned if username is empty', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders?username=&password=password', {
+    headers: requestHeaders
+  })
+  expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+})
+test('GET: Correct error code is returned if  password is empty', async ({ request }) => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders?username=username&password=', {
+    headers: requestHeaders
+  })
+  expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+})
+
+test('PUT: Correct return code is returned in case orderID is out of range (BAD_REQUEST) - AXIOS', async () => {
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'test',
+    customerPhone: 'test',
+    comment: 'test',
+    id: 0,
+  }
+
+  try {
+    await axios.put('https://backend.tallinn-learning.ee/test-orders/11', requestBody, {
+      headers: requestHeaders,
+    })
+  } catch (error) {
+    const axiosError = error as AxiosError // Casting error to AxiosError
+    if (axiosError.response) {
+      expect(axiosError.response.status).toBe(StatusCodes.BAD_REQUEST)
+    }
+  }
+})
+
+
+
