352ea1737278b30081ec1a9561ed1f46f9bb7ad8
alekseigolov <lelik.llk@gmail.com>
2024-10-31 20:53:35 +0200

    first commit

--- tests/auth-order-creation.spec.ts
+++ tests/auth-order-creation.spec.ts
@@ -6,5 +6,6 @@ import { ApiClient } from './api/api-client'
 
 test('Correct username and password should return correct error code 200', async ({ request }) => {
   const apiClient = await ApiClient.getInstance(request) //already authorized
-  await apiClient.createOrderAndReturnOrderId()
+  const orderId = await apiClient.createOrderAndReturnOrderId()
+  expect.soft(orderId).toBeDefined()
 })
