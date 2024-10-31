2e9ce68bda1b4706898176b4bc3ca9459d1a11b2
alekseigolov <lelik.llk@gmail.com>
2024-10-31 20:54:52 +0200

    first commit

--- tests/auth-order-creation.spec.ts
+++ tests/auth-order-creation.spec.ts
@@ -1,7 +1,4 @@
 import { expect, test } from '@playwright/test'
-import { StatusCodes } from 'http-status-codes'
-import { LoginDto } from './dto/login-dto'
-import { OrderDto } from './dto/order-dto'
 import { ApiClient } from './api/api-client'
 
 test('Correct username and password should return correct error code 200', async ({ request }) => {
